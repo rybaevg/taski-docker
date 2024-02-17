7efb1f58122d5e4e7e6e631f09bdd7d36cc418ed
rybaevg <138581190+rybaevg@users.noreply.github.com>
2024-02-17 18:07:52 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,18 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv("SECRET_KEY", 'default_key')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
 
 # Application definition
 
